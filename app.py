f0ed3ea87270ad2fd95244668c180ca0276baed7
Gauri-dsml23 <129536325+Gauri-dsml23@users.noreply.github.com>
2024-08-28 17:11:49 +0530

    Build App first
    Build both Car Price and Stock price prediction. And then publish on git.
    
    Likewise U can create apps using : Streamlit!

---
+++ app.py
@@ -0,0 +1,17 @@
+# import module
+import streamlit as st
+
+# Title
+st.title("This is the title : GauriK, is a Data Scientist & ML & Data engg. too bro...u overthere!!")
+
+# Header
+st.header("This is the header, Gauri K")
+
+# Subheader
+st.subheader("This is the subheader, Gauri a AI professional!")
+
+# Text
+st.text("This is the text, softwear architect!")
+
+# Markdown
+st.markdown("This is the markdown")
